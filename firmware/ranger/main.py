38687feaf3c7da5330f6db13ac6a81dbe9ce2fba
Dylan Kane <dylmkane@gmail.com>
2023-12-27 11:20:14 -0500

    ya

--- firmware/ranger/main.py
+++ firmware/ranger/main.py
@@ -95,7 +95,7 @@ class ButtonHandler:
                     # "-d", "0",       # Duration set to 0 for indefinite recording
                     # "-c", "2",       # 2 channels (stereo)
                     # "-r", "44100",   # Sampling rate 44100 Hz
-                    "-q",  # Suppress standard output
+                    # "-q",  # Suppress standard output
                     "--overwrite",  # Overwrite the file if it exists
                     "output.wav",  # Output file
                 ],
